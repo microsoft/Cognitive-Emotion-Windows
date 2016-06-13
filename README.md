87e07997663193450c1d74522139c7bea938c2ba
Delong Fu <delongfu@gmail.com>
2016-06-13 16:31:40 -0700

    moved Emotion windows projects into new repo

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# Cognitive-Emotion-Windows
-Windows SDK for the Microsoft Emotion API, part of Cognitive Services
+The client library
+==================
+
+The Emotion API client library is a thin C\# client wrapper for Microsoft Cognitive Services (formerly Project Oxford) Emotion
+REST APIs.  
+
+The easiest way to use this client library is to get microsoft.projectoxford.emotion package from [nuget](<http://nuget.org>).
+
+Please go to [Emotion API Package in nuget](https://www.nuget.org/packages/Microsoft.ProjectOxford.Emotion/) for more details.
+
+Order expressions
+============
+
+You can call the function ToRankedList from the Scores class, for example:
+
+```
+IEnumerable<KeyValuePair<string, float>> collection = myScores.ToRankedList();
+```
+
+The sample
+==========
+
+This sample is a Windows WPF application to demonstrate the use of Microsoft Cognitive Services Emotion API.
+
+It demonstrates emotion API by specifying a URL or using a stream.
+
+Build the sample
+----------------
+
+1.  Starting in the folder where you clone the repository (this folder)
+
+2.  Pull in the shared Windows code by calling `git submodule init`
+
+3.  Start Microsoft Visual Studio 2015 and select `File > Open >
+    Project/Solution`.
+
+4.  Go to `Sample-WPF Folder`.
+
+5.  Double-click the Visual Studio 2015 Solution (.sln) file
+    EmotionAPI-WPF-Samples.
+
+6.  Press Ctrl+Shift+B, or select `Build > Build Solution`.
+
+Run the sample
+--------------
+
+After the build is complete, press F5 to run the sample.
+
+First, you must obtain a Emotion API subscription key by following instructions in [Microsoft Cognitive Services subscription](<https://www.microsoft.com/cognitive-services/en-us/sign-up>).
+
+Locate the text edit box saying "Paste your subscription key here to start" on
+the top right corner. Paste your subscription key. You can choose to persist
+your subscription key in your machine by clicking "Save Key" button. When you
+want to delete the subscription key from the machine, click "Delete Key" to
+remove it from your machine.
+
+Click on "Select Scenario" to use samples of different scenarios, and
+follow the instructions on screen.
+
+Microsoft will receive the images you upload and may use them to improve Emotion
+API and related services. By submitting an image, you confirm you have consent
+from everyone in it.
+
+<img src="SampleScreenshots/SampleRunning.png" width="80%"/>
+
+Contributing
+============
+We welcome contributions and are always looking for new SDKs, input, and
+suggestions. Feel free to file issues on the repo and we'll address them as we can. You can also learn more about how you can help on the [Contribution
+Rules & Guidelines](</CONTRIBUTING.md>).
+
+For questions, feedback, or suggestions about Microsoft Cognitive Services, feel free to reach out to us directly.
+
+-   [Cognitive Services UserVoice Forum](<https://cognitive.uservoice.com>)
+
+License
+=======
+
+All Microsoft Cognitive Services SDKs and samples are licensed with the MIT License. For more details, see
+[LICENSE](</LICENSE.md>).
+
+Sample images are licensed separately, please refer to [LICENSE-IMAGE](</LICENSE-IMAGE.md>).
---
+++ Sample-WPF/MainWindow.xaml.cs
@@ -0,0 +1,248 @@
+﻿// 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// 
+// Microsoft Cognitive Services (formerly Project Oxford): https://www.microsoft.com/cognitive-services
+// 
+// Microsoft Cognitive Services (formerly Project Oxford) GitHub:
+// https://github.com/Microsoft/ProjectOxford-ClientSDK
+// 
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+// 
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using SampleUserControlLibrary;
+using Microsoft.ProjectOxford.Emotion.Contract;
+
+namespace EmotionAPI_WPF_Samples
+{
+    public class EmotionResultDisplayItem
+    {
+        public Uri ImageSource { get; set; }
+
+        public System.Windows.Int32Rect UIRect { get; set; }
+        public string Emotion1 { get; set; }
+        public string Emotion2 { get; set; }
+        public string Emotion3 { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SampleScenarios ScenarioControl
+        {
+            get
+            {
+                return _scenariosControl;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //
+            // Initialize SampleScenarios User Control with titles and scenario pages
+            //
+            _scenariosControl.SampleTitle = "Emotion API";
+            _scenariosControl.SampleScenarioList = new Scenario[]
+            {
+                new Scenario { Title = "Detect emotion using a stream", PageClass = typeof(DetectEmotionUsingStreamPage) },
+                new Scenario { Title = "Detect emotion using a URL", PageClass = typeof(DetectEmotionUsingURLPage)},
+                new Scenario { Title = "Detect emotion in a Video", PageClass = typeof(EmotionDetectionUsingVideoPage) }
+            };
+        }
+
+        public void Log(string message)
+        {
+            _scenariosControl.Log(message);
+        }
+
+        public void LogEmotionResult(Emotion[] emotionResult)
+        {
+            int emotionResultCount = 0;
+            if (emotionResult != null && emotionResult.Length > 0)
+            {
+                foreach (Emotion emotion in emotionResult)
+                {
+                    Log("Emotion[" + emotionResultCount + "]");
+                    Log("  .FaceRectangle = left: " + emotion.FaceRectangle.Left
+                             + ", top: " + emotion.FaceRectangle.Top
+                             + ", width: " + emotion.FaceRectangle.Width
+                             + ", height: " + emotion.FaceRectangle.Height);
+
+                    Log("  Anger    : " + emotion.Scores.Anger.ToString());
+                    Log("  Contempt : " + emotion.Scores.Contempt.ToString());
+                    Log("  Disgust  : " + emotion.Scores.Disgust.ToString());
+                    Log("  Fear     : " + emotion.Scores.Fear.ToString());
+                    Log("  Happiness: " + emotion.Scores.Happiness.ToString());
+                    Log("  Neutral  : " + emotion.Scores.Neutral.ToString());
+                    Log("  Sadness  : " + emotion.Scores.Sadness.ToString());
+                    Log("  Surprise  : " + emotion.Scores.Surprise.ToString());
+                    Log("");
+                    emotionResultCount++;
+                }
+            }
+            else
+            {
+                Log("No emotion is detected. This might be due to:\n" +
+                    "    image is too small to detect faces\n" +
+                    "    no faces are in the images\n" +
+                    "    faces poses make it difficult to detect emotions\n" +
+                    "    or other factors");
+            }
+        }
+
+        public void DrawFaceRectangle(Image image, BitmapImage bitmapSource, Emotion[] emotionResult)
+        {
+            if (emotionResult != null && emotionResult.Length > 0)
+            {
+                DrawingVisual visual = new DrawingVisual();
+                DrawingContext drawingContext = visual.RenderOpen();
+
+                drawingContext.DrawImage(bitmapSource,
+                    new Rect(0, 0, bitmapSource.Width, bitmapSource.Height));
+
+                double dpi = bitmapSource.DpiX;
+                double resizeFactor = 96 / dpi;
+
+                foreach (var emotion in emotionResult)
+                {
+                    Microsoft.ProjectOxford.Common.Rectangle faceRect = emotion.FaceRectangle;
+
+                    drawingContext.DrawRectangle(
+                        Brushes.Transparent,
+                        new Pen(Brushes.Cyan, 4),
+                        new Rect(
+                            faceRect.Left * resizeFactor,
+                            faceRect.Top * resizeFactor,
+                            faceRect.Width * resizeFactor,
+                            faceRect.Height * resizeFactor)
+                    );
+                }
+
+                drawingContext.Close();
+
+                RenderTargetBitmap faceWithRectBitmap = new RenderTargetBitmap(
+                    (int)(bitmapSource.PixelWidth * resizeFactor),
+                    (int)(bitmapSource.PixelHeight * resizeFactor),
+                    96,
+                    96,
+                    PixelFormats.Pbgra32);
+
+                faceWithRectBitmap.Render(visual);
+
+                image.Source = faceWithRectBitmap;
+            }
+        }
+
+
+        public void ListEmotionResult(Uri imageUri, ListBox resultListBox, Emotion[] emotionResult)
+        {
+            if (emotionResult != null)
+            {
+                EmotionResultDisplay[] resultDisplay = new EmotionResultDisplay[8];
+                List<EmotionResultDisplayItem> itemSource = new List<EmotionResultDisplayItem>();
+                for (int i = 0; i < emotionResult.Length; i++)
+                {
+                    Emotion emotion = emotionResult[i];
+                    resultDisplay[0] = new EmotionResultDisplay { EmotionString = "Anger", Score = emotion.Scores.Anger };
+                    resultDisplay[1] = new EmotionResultDisplay { EmotionString = "Contempt", Score = emotion.Scores.Contempt };
+                    resultDisplay[2] = new EmotionResultDisplay { EmotionString = "Disgust", Score = emotion.Scores.Disgust };
+                    resultDisplay[3] = new EmotionResultDisplay { EmotionString = "Fear", Score = emotion.Scores.Fear };
+                    resultDisplay[4] = new EmotionResultDisplay { EmotionString = "Happiness", Score = emotion.Scores.Happiness };
+                    resultDisplay[5] = new EmotionResultDisplay { EmotionString = "Neutral", Score = emotion.Scores.Neutral };
+                    resultDisplay[6] = new EmotionResultDisplay { EmotionString = "Sadness", Score = emotion.Scores.Sadness };
+                    resultDisplay[7] = new EmotionResultDisplay { EmotionString = "Surprise", Score = emotion.Scores.Surprise };
+
+                    Array.Sort(resultDisplay, CompareDisplayResults);
+
+                    String[] emotionStrings = new String[3];
+                    for (int j = 0; j < 3; j++)
+                    {
+                        emotionStrings[j] = resultDisplay[j].EmotionString + ":" + resultDisplay[j].Score.ToString("0.000000"); ;
+                    }
+
+                    itemSource.Add(new EmotionResultDisplayItem
+                    {
+                        ImageSource = imageUri,
+                        UIRect = new Int32Rect(emotion.FaceRectangle.Left, emotion.FaceRectangle.Top, emotion.FaceRectangle.Width, emotion.FaceRectangle.Height),
+                        Emotion1 = emotionStrings[0],
+                        Emotion2 = emotionStrings[1],
+                        Emotion3 = emotionStrings[2]
+                    });
+                }
+                resultListBox.ItemsSource = itemSource;
+            }
+        }
+
+        public void ListVideoEmotionResult(ListBox resultListBox, VideoAggregateEvent aggregateEvent)
+        {
+            if (aggregateEvent != null)
+            {
+                EmotionResultDisplay[] resultDisplay = new EmotionResultDisplay[8];
+                List<EmotionResultDisplayItem> itemSource = new List<EmotionResultDisplayItem>();
+
+                Scores meanScores = aggregateEvent.WindowMeanScores;
+                resultDisplay[0] = new EmotionResultDisplay { EmotionString = "Anger", Score = meanScores.Anger, OriginalIndex = 0 };
+                resultDisplay[1] = new EmotionResultDisplay { EmotionString = "Contempt", Score = meanScores.Contempt, OriginalIndex = 1 };
+                resultDisplay[2] = new EmotionResultDisplay { EmotionString = "Disgust", Score = meanScores.Disgust, OriginalIndex = 2 };
+                resultDisplay[3] = new EmotionResultDisplay { EmotionString = "Fear", Score = meanScores.Fear, OriginalIndex = 3 };
+                resultDisplay[4] = new EmotionResultDisplay { EmotionString = "Happiness", Score = meanScores.Happiness, OriginalIndex = 4 };
+                resultDisplay[5] = new EmotionResultDisplay { EmotionString = "Neutral", Score = meanScores.Neutral, OriginalIndex = 5 };
+                resultDisplay[6] = new EmotionResultDisplay { EmotionString = "Sadness", Score = meanScores.Sadness, OriginalIndex = 6 };
+                resultDisplay[7] = new EmotionResultDisplay { EmotionString = "Surprise", Score = meanScores.Surprise, OriginalIndex = 7 };
+
+                Array.Sort(resultDisplay, CompareDisplayResults);
+
+                for (int j = 0; j < 3; j++)
+                {
+                    string emotion = resultDisplay[j].EmotionString + ":" + resultDisplay[j].Score.ToString("0.000000");
+
+                    itemSource.Add(new EmotionResultDisplayItem
+                    {
+                        ImageSource = new Uri("pack://application:,,,/EmotionAPI-WPF-Samples;component/Assets/Emotions.png"),
+                        UIRect = new Int32Rect(resultDisplay[j].OriginalIndex * 96, 0, 96, 96),
+                        Emotion1 = emotion
+                    });
+                }
+
+                resultListBox.ItemsSource = itemSource;
+            }
+        }
+
+        private int CompareDisplayResults(EmotionResultDisplay result1, EmotionResultDisplay result2)
+        {
+            return ((result1.Score == result2.Score) ? 0 : ((result1.Score < result2.Score) ? 1 : -1));
+        }
+         
+    }
+}
